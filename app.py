a1a1bce9fe4c53fd114894b6467df9c18a93c9ff
Unknown <nizar.chtioui@uit.ac.ma>
2019-12-01 20:36:37 +0100

    modif app env

--- app.py
+++ app.py
@@ -15,12 +15,11 @@ from flask import Flask, request, render_template, g, jsonify
 from datetime import date, timedelta
 from src.api.WeatherMANGER import WeatherManager
 
-port = int(os.environ.get('PORT', 5000))
 
 
 # create app
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 @app.route('/')
 def index():
     current_ip = WeatherManager.get_ip()
@@ -28,4 +27,7 @@ def index():
     temper = WeatherManager.getWeatherByCountry(country)
     return render_template('index.html', country=country, C_Wearher = temper)
 
-app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+        import os  
+        port = int(os.environ.get('PORT', 5000)) 
+        app.run(host='0.0.0.0', port=port)
